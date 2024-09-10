fef9dd88fa47aed48df28cfdc5e9ae92add547e6
JeanetteSchwarz <jeanette.schwarz@t-online.de>
2024-09-10 14:15:24 +0200

    Initial commit

---
+++ src/main/java/org/example/springservicezoo/SpringServiceZooApplication.java
@@ -0,0 +1,13 @@
+package org.example.springservicezoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServiceZooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServiceZooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springservicezoo/SpringServiceZooApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springservicezoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceZooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
